785772968b77d8922c442de57be5bc8bbd5a0078
dw <dw@de>
2016-04-16 13:29:38 +0300

    count-neighbours

--- Home/count-neighbours.py
+++ Home/count-neighbours.py
@@ -1,9 +1,22 @@
 def count_neighbours(grid, row, col):
-    return 0
+    count = 0
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(row - 1, row + 2):
+        if 0 <= i < rows:
+            for j in range(col - 1, col + 2):
+                if 0 <= j < cols:
+                    if grid[i][j] > 0:
+                        count += 1
+    count -= grid[row][col]
+
+    return count
 
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    ''' # These "asserts" using only for self-checking and not necessary for auto-testing
     assert count_neighbours(((1, 0, 0, 1, 0),
                              (0, 1, 0, 0, 0),
                              (0, 0, 1, 0, 1),
@@ -13,7 +26,7 @@ if __name__ == '__main__':
                              (0, 1, 0, 0, 0),
                              (0, 0, 1, 0, 1),
                              (1, 0, 0, 0, 0),
-                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example" '''
     assert count_neighbours(((1, 1, 1),
                              (1, 1, 1),
                              (1, 1, 1),), 0, 2) == 3, "Dense corner"
